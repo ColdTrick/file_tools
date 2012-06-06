0c3b91571e0252ac250dc94641c6c09c7ce02613
Jerome Bakker <jeabakker@coldtrick.com>
2012-06-06 09:27:06 +0200

    fixed: page_handler logic

--- lib/page_handlers.php
+++ lib/page_handlers.php
@@ -1,5 +1,8 @@
 <?php
+
 	function file_tools_page_handler($page) {
+		$include_file = false;
+		
 		switch($page[0]) {
 			case "list":
 				if(elgg_is_xhr() && !empty($page[1])) {
@@ -13,48 +16,47 @@
 						set_input("folder_guid", $page[2]);
 					}
 					
-					include(dirname(dirname(__FILE__)) . "/pages/list.php");
-					break;
+					$include_file = dirname(dirname(__FILE__)) . "/pages/list.php";
 				}
-			case "reorder":
-				include(dirname(dirname(__FILE__)) . "/procedures/reorder.php");
 				break;
 			case "folder":
 				if($page[1] == 'new') {
 					if(!empty($page[2])) {
 						elgg_set_page_owner_guid($page[2]);
 					}
-					include(dirname(dirname(__FILE__)) . "/pages/folder/new.php");
-					break;
+					$include_file = dirname(dirname(__FILE__)) . "/pages/folder/new.php";
 				} elseif($page[1] == 'edit') {
 					if(!empty($page[2])) {
 						set_input("folder_guid", $page[2]);
 	
-						include(dirname(dirname(__FILE__)) . "/pages/folder/edit.php");
-						break;
+						$include_file = dirname(dirname(__FILE__)) . "/pages/folder/edit.php";
 					}
 				}
+				break;
 			case "file":
 				if($page[1] == 'new') {
 					if(!empty($page[2])) {
 						elgg_set_page_owner_guid($page[2]);
 					}
-					include(dirname(dirname(__FILE__)) . "/pages/file/new.php");
+					$include_file = dirname(dirname(__FILE__)) . "/pages/file/new.php";
 				} elseif($page[1] == 'download') {
-					include(dirname(dirname(__FILE__)) . "/pages/file/download.php");
+					$include_file = dirname(dirname(__FILE__)) . "/pages/file/download.php";
 				}
 				break;
 			case "proc":
 				if(file_exists(dirname(dirname(__FILE__)) . "/procedures/" . $page[1] . "/" . $page[2] . ".php")) {
-					include(dirname(dirname(__FILE__)) . "/procedures/" . $page[1] . "/" . $page[2] . ".php");
+					$include_file = dirname(dirname(__FILE__)) . "/procedures/" . $page[1] . "/" . $page[2] . ".php";
 				} else {
 					echo json_encode(array('valid' => 0));
 					exit;
 				}
 				break;
-			default:
-				forward("file/all");
 		}
 	
-		return TRUE;
+		if(!empty($include_file)){
+			include($include_file);
+			return true;
+		} else {
+			forward("file/all");
+		}		
 	}
