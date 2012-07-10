555b5af8784f95eb2afe755d2ee821d6d9e2105a
Matt Beckett <beck24@gmail.com>
2012-07-10 01:56:39 -0600

    fix js concatenation

--- views/default/file_tools/js/site.php
+++ views/default/file_tools/js/site.php
@@ -195,7 +195,7 @@ elgg.file_tools.load_folder = function(folder_guid){
 		var add_link = $('ul.elgg-menu-title li.elgg-menu-item-add a').attr("href");
 
 		var path = elgg.parse_url(add_link, "path");
-		var new_add_link = elgg.get_site_url() + 'file/add/' . elgg.get_page_owner_guid() + "?folder_guid=" + folder_guid;
+		var new_add_link = elgg.get_site_url() + 'file/add/' + elgg.get_page_owner_guid() + "?folder_guid=" + folder_guid;
 		
 		$('ul.elgg-menu-title li.elgg-menu-item-add a').attr("href", new_add_link);
 	});
